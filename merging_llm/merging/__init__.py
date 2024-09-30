a50013db766a6c4f07e2a4b45302c78116569be7
jeanpetitt <jeanpetityvelos@gmail.com>
2024-09-30 13:49:31 +0100

    init project

---
+++ merging_llm/merging/__init__.py
@@ -0,0 +1,5 @@
+from merging_llm.merging.LlamaAvg import LlamaAvg
+from merging_llm.merging.TinyLlamaAvg import TinyLlamaAvg
+from merging_llm.merging.FlanT5Avg import FlanT5Avg
+
+## TODO Add more merge methods here
